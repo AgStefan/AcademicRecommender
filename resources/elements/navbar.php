fd031fce2763d8e7a7b268de0fc041dd6ac592d8
stefa <stefan.agape97@gmail.com>
2018-03-17 13:54:07 +0200

    Float modified to right in navbar-wrapper li. Added font-family and font-size to navbar-wrapper li a. Changed opacity and background-color in avbar-wrapper li a:hover active.

--- resources/elements/navbar.php
+++ resources/elements/navbar.php
@@ -2,11 +2,11 @@
 
 
         <ul>
-            <li class="current"><a href="#Register">Register</a></li>
-            <li><a href="#Login">Login</a></li>
-            <li><a href="#Logout">Logout</a></li>
-            <li><a href="#FAQ">FAQ</a></li>
             <li style="float: right"><a href="#About">About</a></li>
+            <li><a href="#FAQ">FAQ</a></li>
+            <li><a href="#Logout">Logout</a></li>
+            <li><a href="#Login">Login</a></li>
+            <li class="current"><a href="#Register">Register</a></li>
         </ul>
 
 </div>
