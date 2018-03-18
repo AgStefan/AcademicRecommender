1bcdc6aed6c1a7aaf0b7061d719991d6808338cc
Carja Mihail <carja_mihail@yahoo.com>
2018-03-18 20:36:50 +0200

    Disciplines php +disciplines css.

--- resources/views/Disciplines.php
+++ resources/views/Disciplines.php
@@ -1,1 +1,46 @@
-<h1>Disciplines</h1>
+<?php require_once('./../resources/elements/header.php') ?>
+<div class="content-right">
+    <div class="disciplineswrapper">
+         <div class="disciplinewrapper">
+         <h2>Nume Disciplina</h2>
+             <div class="detailswrapper">
+                 <p>Rating</p>
+                 <p>Profesor</p>
+                 <p>an</p>
+
+             </div>
+         </div>
+        <div class="disciplinewrapper">
+            <h2>Nume Disciplina</h2>
+            <div class="detailswrapper">
+                <p>Rating</p>
+                <p>Profesor</p>
+                <p>an</p>
+
+            </div>
+        </div>
+        <div class="disciplinewrapper">
+            <h2>Nume Disciplina</h2>
+            <div class="detailswrapper">
+                <p>Rating</p>
+                <p>Profesor</p>
+                <p>an</p>
+
+            </div>
+        </div>
+        <div class="disciplinewrapper">
+            <h2>Nume Disciplina</h2>
+            <div class="detailswrapper">
+                <p>Rating</p>
+                <p>Profesor</p>
+                <p>an</p>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+</div>
+<?php require_once ("./../resources/elements/footer.php"); ?>
