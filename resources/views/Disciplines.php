8344c7258fa99a54ab7af0913739bf482bc6ac74
Carja Mihail <carja_mihail@yahoo.com>
2018-03-19 20:20:25 +0200

    disciplines rebuild

--- resources/views/Disciplines.php
+++ resources/views/Disciplines.php
@@ -1,43 +1,95 @@
 <?php require_once('./../resources/elements/header.php') ?>
 <div class="content-right">
     <div class="disciplineswrapper">
-         <div class="disciplinewrapper">
-         <h2>Nume Disciplina</h2>
-             <div class="detailswrapper">
-                 <p>Rating</p>
-                 <p>Profesor</p>
-                 <p>an</p>
+<!--        <div class="postwrapper">-->
+<!--            <h1> Popular post </h1>-->
+<!--        </div>-->
 
-             </div>
-         </div>
-        <div class="disciplinewrapper">
-            <h2>Nume Disciplina:
-            <span  class="Ratting"> Logica</span>
-            </h2>
-
-            <h2>Rating:
-                <span  class="Ratting"> 3.08</span>
-            </h2>
-
+        <div class="disciplines-list">
+            <div class="disciplinewrapper">
+                <div class="discipline-image">
+                    <img src="./images/demo-image.png" alt="">
+                </div>
+                <div class="discipline-information">
+                    <h2>Nume Disciplina:<span  class="Ratting"> Logica</span></h2>
+                    <div class="discipline-extra-information">
+                        <span>Rating: 3.0 / </span>
+                        <span>3 Comments / </span>
+                        <span>Other info</span>
+                    </div>
+                    <div class="discipline-small-description">
+                        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem v...
+                    </div>
+                    <div class="discipline-read-more-button-wrapper">
+                        <a href="/discipline/matematica">Read more</a>
+                    </div>
+                </div>
             </div>
+
+            <div class="disciplinewrapper">
+                <div class="discipline-image">
+                    <img src="./images/demo-image.png" alt="">
+                </div>
+                <div class="discipline-information">
+                    <h2>Nume Disciplina:<span  class="Ratting"> Logica</span></h2>
+                    <div class="discipline-extra-information">
+                        <span>Rating: 3.0 / </span>
+                        <span>3 Comments / </span>
+                        <span>Other info</span>
+                    </div>
+                    <div class="discipline-small-description">
+                        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem v...
+                    </div>
+                    <div class="discipline-read-more-button-wrapper">
+                        <a href="/discipline/matematica">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="disciplinewrapper">
+                <div class="discipline-image">
+                    <img src="./images/demo-image.png" alt="">
+                </div>
+                <div class="discipline-information">
+                    <h2>Nume Disciplina:<span  class="Ratting"> Logica</span></h2>
+                    <div class="discipline-extra-information">
+                        <span>Rating: 3.0 / </span>
+                        <span>3 Comments / </span>
+                        <span>Other info</span>
+                    </div>
+                    <div class="discipline-small-description">
+                        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem v...
+                    </div>
+                    <div class="discipline-read-more-button-wrapper">
+                        <a href="/discipline/matematica">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="disciplinewrapper">
+                <div class="discipline-image">
+                    <img src="./images/demo-image.png" alt="">
+                </div>
+                <div class="discipline-information">
+                    <h2>Nume Disciplina:<span  class="Ratting"> Logica</span></h2>
+                    <div class="discipline-extra-information">
+                        <span>Rating: 3.0 / </span>
+                        <span>3 Comments / </span>
+                        <span>Other info</span>
+                    </div>
+                    <div class="discipline-small-description">
+                        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem v...
+                    </div>
+                    <div class="discipline-read-more-button-wrapper">
+                        <a href="/discipline/matematica">Read more</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <div class="disciplinewrapper">
-            <h2>Nume Disciplina</h2>
-            <div class="detailswrapper">
-                <p>Rating</p>
-                <p>Profesor</p>
-                <p>an</p>
 
-            </div>
-        </div>
-        <div class="disciplinewrapper">
-            <h2>Nume Disciplina</h2>
-            <div class="detailswrapper">
-                <p>Rating</p>
-                <p>Profesor</p>
-                <p>an</p>
+        <div class="disciplines-popular">
 
-            </div>
         </div>
     </div>
 
