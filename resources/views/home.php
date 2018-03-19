e14bb24fdd57040a7eefe74ff5cc6db9cb12030d
stefa <stefan.agape97@gmail.com>
2018-03-20 00:43:21 +0200

    Added logo and project name to home page. Added home.css for home page.

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,7 +1,11 @@
 <?php require_once ("./../resources/elements/header.php"); ?>
-
+<link rel="stylesheet" type="text/css" href="/css/home.css"/>
 <div class="content-right">
-    <div>Home</div>
+    <div>
+        <img class="image-description" src="./images/academic_logo.jpg"/>
+    </div>
 </div>
 
 <?php require_once ("./../resources/elements/footer.php"); ?>
+
+
