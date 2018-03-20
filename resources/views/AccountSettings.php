c0258cf4b39dc21c6491332c72a848e728d20784
Carja Mihail <carja_mihail@yahoo.com>
2018-03-20 02:14:02 +0200

    html errors fixing

--- resources/views/AccountSettings.php
+++ resources/views/AccountSettings.php
@@ -5,41 +5,43 @@
 
 <div class="AccountSettings">
 
-    <h1><img class="image2"  src="./images/login_icon.png"</img>Account Settings</h1>
+    <h1>
+        <img class="image2"  src="./images/login_icon.png" alt="">
+        Account Settings</h1>
 
     <form class="wrapper-input">
         <hr>
 
         <label for="username">
-            <img class="image1"  src="./images/username.png"</img>
+            <img class="image1"  src="./images/username.png" alt="">
             <span>Username</span>
         </label>
         <input id="username" type="text" placeholder="Enter username">
 
 
         <label for="passwordchange">
-            <img class="image1"  src="./images/password_icon.png"</img>
+            <img class="image1"  src="./images/password_icon.png" alt="">
             <span>Password</span>
         </label>
         <input id="passwordchange" type="password" placeholder="Enter new password">
 
 
         <label for="repeatpasswordchange">
-            <img class="image1"  src="./images/password_icon.png"</img>
+            <img class="image1"  src="./images/password_icon.png" alt="">
             <span>Reenter Password</span>
         </label>
         <input id="repeatpasswordchange1" type="password" placeholder="Reenter new password">
 
 
         <label for="emailchange">
-            <img class="image1"  src="./images/email_icon.jpg"</img>
+            <img class="image1"  src="./images/email_icon.jpg" alt="">
             <span>Email</span>
         </label>
         <input id="emailchange" type="email" placeholder="Enter new email">
 
 
         <label for="telephonechange">
-            <img class="image1"  src="./images/telephone.jpg"</img>
+            <img class="image1"  src="./images/telephone.jpg" alt="">
             <span>Telephone</span>
         </label>
         <input id="telephonechange" type="text" placeholder="Enter new telephone number">
