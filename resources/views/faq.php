22f96d9f1cb4182afb0e26e7a8c0d79c69ddea0e
stefa <stefan.agape97@gmail.com>
2018-06-13 23:36:37 +0300

    Added Add Question functionality for user, Delete and Edit for ADMIN (finished)

--- resources/views/faq.php
+++ resources/views/faq.php
@@ -8,7 +8,7 @@ require_once('./../resources/elements/header.php') ?>
 <div class="faq-wrapper">
 
     <p class="HaveaQ">
-       <b style="padding-left: 80px"> Have a Question? </b>
+       <b> Have a Question? </b>
 
     </p>
     <form method="POST" action="search-action">
@@ -22,7 +22,7 @@ require_once('./../resources/elements/header.php') ?>
 
     </form>
     <p class="HaveaQ">
-        <b style="padding-left: 80px">Want to add a question? </b>
+        <b>Want to add a question? </b>
 
     </p>
 
@@ -35,36 +35,90 @@ require_once('./../resources/elements/header.php') ?>
             </button>
 
         </form>
+
+    <?php
+    if (isset($_SESSION) && isset($_SESSION['role']) && $_SESSION['role'] == '2' ){
+
+        ?>
+        <p class="HaveaQ">
+            <b>ADMIN: </b>
+        </p>
+        <button onclick="removeQuestion()" type="button2" class="removeButton">Delete a question by ID</button>
+        <form style="display:none;" method="POST" id="removeForm"  action="/removeQuestion-action">
+
+            <input id="id" name="id" type="text"  required placeholder="Enter the ID">
+            <div class="button-wrapper">
+                <input type="submit" id="buttonRemove" name="buttonRemove" value="Delete Question" />
+            </div>
+        </form>
+            <br><br>
+        <button onclick="addAnswer()" type="button2" class="addAnswerButton">Delete a question by ID</button>
+
+            <form style="display:none;" method="POST" id="answerForm"  action="/addAnswer-action">
+                <input id="id" name="id" type="text"  required placeholder="Enter the ID">
+                <input id="answer" name="answer" type="text"  required placeholder="Enter the answer"/>
+                <div class="button-wrapper">
+                    <input type="submit" id="buttonAnswer" name="buttonAnswer" value="Insert Answer" />
+                </div>
+
+            </form>
+
+
+
+
+        <?php
+    }
+    ?>
     <p class="Questions">
         <b > Most common questions: </b>
-
     </p>
 
 
-        <?php foreach ($questions as $question): ?>
+        <?php
+        if (is_null($questions)){
+
+        }
+        else{
+
+        foreach ($questions as $question): ?>
     <div class="faq_holder">
+
+
         <?php
         if (isset($_SESSION) && isset($_SESSION['role']) && $_SESSION['role'] == '2' ){
 
             ?>
-            <form method="POST" action="removeQuestion-action">
-             <div class="button-wrapper">
-            <input type="submit" id="buttonRemove" name="buttonRemove" value="Delete Question" />
-        </div>
-
-            </form>
+            <p><b>ID: <?= $question->id ?></b></p>
             <?php
         }
         ?>
-    <img class="QuestionIcon" src="./images/QuestionIcon.png" alt="">
+
         <p><b> Q: </b> <?= $question->question ?></p>
 
         <p><b> A: </b> <?= $question->answer ?></p>
-
-
     </div>
-    <?php endforeach; ?>
+    <?php endforeach;} ?>
 
 </div>
 
 <?php require_once('./../resources/elements/footer.php') ?>
+
+        <script>
+            function removeQuestion() {
+                var x = document.getElementById("removeForm");
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                }
+            }
+
+            function addAnswer() {
+                var x = document.getElementById("answerForm");
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                }
+            }
+        </script>
