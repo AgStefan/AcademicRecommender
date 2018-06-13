98fb43a7241b476949a4447ebd98929c435fb69a
ancabej <ancabejinariu16@gmail.com>
2018-06-13 20:36:23 +0300

    Created messages form + functionality

--- resources/views/disciplines.php
+++ resources/views/disciplines.php
@@ -1,50 +1,98 @@
-<?php
-
-require_once('./../resources/elements/header.php') ?>
+<?php require_once('./../resources/elements/header.php') ?>
 <div class="content-right">
-
-    <!-- Add/Remove a discipline - only for the admin -->
-    <?php
-
-    if (isset($_SESSION) && isset($_SESSION['role']) && $_SESSION['role'] == '2' ) {
-        require_once('adminDisciplines.php');
-    }
-
-    ?>
-
-
-
-
     <div class="disciplineswrapper">
 
-        <?php foreach ($disciplines as $discipline): ?>
             <div class="disciplinewrapper">
                 <div class="discipline-image">
                     <img src="./images/web-technologies.png" alt="">
                 </div>
                 <div class="discipline-information">
                     <h2 >Discipline:
-                        <span ><?= $discipline->nume ?></span></h2>
+                        <span >Web Technologies</span></h2>
                     <div class="discipline-extra-information">
                         <button class="btn">Rating: 3.4 </button>
                         <span>/</span>
                         <button class="btn">3 Comments</button>
                         <span>/</span>
-                        <button class="btn">An: <?= $discipline->an ?></button>
-                        <span>/</span>
-
+                        <button class="btn">Other info</button>
                     </div>
 
                     <div class="discipline-small-description">
-                        <?= $discipline->description ?>
+                        Web technologies is a general term referring to the many languages and multimedia packages that are used in conjunction with one another, to produce dynamic web sites such as this one.
                     </div>
                     <div class="discipline-read-more-button-wrapper">
-                        <a href="/discipline/<?= $discipline->slug ?>">Read more</a>
+                        <a href="/matematica">Read more</a>
                     </div>
                 </div>
             </div>
 
-        <?php endforeach; ?>
+            <div class="disciplinewrapper">
+                <div class="discipline-image">
+                    <img src="./images/data-base.png" alt="">
+                </div>
+                <div class="discipline-information">
+                    <h2>Discipline:<span  class="Ratting"> Data bases</span></h2>
+                    <div class="discipline-extra-information">
+                        <button class="btn">Rating: 3.4 </button>
+                        <span>/</span>
+                        <button class="btn">3 Comments</button>
+                        <span>/</span>
+                        <button class="btn">Other info</button>
+                    </div>
+                    <div class="discipline-small-description">
+                        A databyase is an organized collection of data. A relational database, more restrictively, is a collection of schemas, tables, queries, reports, views, and other elements.
+                    </div>
+                    <div class="discipline-read-more-button-wrapper">
+                        <a href="/discipline/matematica">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="disciplinewrapper">
+                <div class="discipline-image">
+                    <img src="./images/machine-learning.png" alt="">
+                </div>
+                <div class="discipline-information">
+                    <h2>Discipline:<span  class="Ratting"> Machine Learning</span></h2>
+                    <div class="discipline-extra-information">
+                        <button class="btn">Rating: 3.4 </button>
+                        <span>/</span>
+                        <button class="btn">3 Comments</button>
+                        <span>/</span>
+                        <button class="btn">Other info</button>
+                    </div>
+                    <div class="discipline-small-description">
+                        Evolved from the study of pattern recognition and computational learning theory in artificial intelligence, machine learning explores the study and construction of algorithms.
+                    </div>
+                    <div class="discipline-read-more-button-wrapper">
+                        <a href="/discipline/matematica">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="disciplinewrapper">
+                <div class="discipline-image">
+                    <img src="./images/oop.png" alt="">
+                </div>
+                <div class="discipline-information">
+                    <h2>Discipline:<span  class="Ratting"> OOP</span></h2>
+                    <div class="discipline-extra-information">
+                        <button class="btn">Rating: 3.4 </button>
+                        <span>/</span>
+                        <button class="btn">3 Comments</button>
+                        <span>/</span>
+                        <button class="btn">Other info</button>
+                    </div>
+                    <div class="discipline-small-description">
+                        Object-oriented programming (OOP) is a programming paradigm based on the concept of "objects", which may contain data, in the form of fields, often known as attributes.
+                    </div>
+                    <div class="discipline-read-more-button-wrapper">
+                        <a href="matematica">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+
 
         <div class="disciplines-popular">
 
